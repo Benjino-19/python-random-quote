04ee87194424754f4ff17f69781795a8cdc60340
Benjino-19 <63230861+Benjino-19@users.noreply.github.com>
2020-04-06 17:27:06 +0200

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
+impor random
 def first():
   # print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
+  last = 13
+  rnd = random.randint(0, last)
 
-  print(quotes[13])
+  print(quotes[rnd])
 
 if __name__== "__main__":
   first()
